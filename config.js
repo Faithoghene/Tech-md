4c134c1ae09fec55b9ba38620557d4c07ecfc600
SagaraSubash <110216100+SagaraSubash@users.noreply.github.com>
2024-08-30 16:16:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "4JJmBB5D#4PEjNRaLl1eFpRE9DWIa6Ch_LYiSdWaAPt6S756_JeY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/xdhMJy1b/Screenshot-20240830-004711-Pixel-Lab.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94760059126\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
-SUDO_NB: process.env.SUDO_NB || "94760059126",
+SUDO_NB: process.env.SUDO_NB || "94768376487",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: "true"
+AUTO_VOICE: "false"
 };
