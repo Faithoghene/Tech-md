ece53c57e4821cf787ef2bccaaa1310d208ad4ee
SagaraSubash <110216100+SagaraSubash@users.noreply.github.com>
2024-08-30 05:30:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9mVVwLBD#g9ivSg1kFuyQpTIcDBBLUyTTGLMsQXfcPibdDzpqADg",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9b44e3266c065df601430.jpg",
+SESSION_ID: process.env.SESSION_ID || "4JJmBB5D#4PEjNRaLl1eFpRE9DWIa6Ch_LYiSdWaAPt6S756_JeY",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/xdhMJy1b/Screenshot-20240830-004711-Pixel-Lab.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94760059126\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
 SUDO_NB: process.env.SUDO_NB || "94760059126",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
